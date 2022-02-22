9c191bcd88a71fea7683626704fa26167ffa0afe
kaueleao <kaue44@yahoo.com.br>
2022-02-22 15:14:06 -0300

    ajuste na logica da funcao para gerar as senhas

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
+//Função para gerar números aleatórios
 function gerar(){
     const aleatorio = Math.floor((Math.random() * 99999999) + 10000000);
-    document.getElementById("resultado").innerHTML = aleatorio;
+    
+    //Condição para verificar se o número gerado não ultrapassa o limite de 8 digítos.
+    if (aleatorio <= 99999999) {
+     
+        document.getElementById("resultado").innerHTML = aleatorio;
+        
+    }
+
+    //Caso o número gerado ultrapasse a condição, ele gera um novo número.
+    else {
+        gerar();
+        console.log('Reprocessou outro número'); 
+    }
 }
 
+//Função utilizada para limpar a tela, caso o usuário queira.
 function limpar(){
     location.reload();
 }
